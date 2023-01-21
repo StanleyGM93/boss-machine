281f55bda32560b825cea12962d46c997207b2b1
Stanley Mair <stanleygmair@gmail.com>
2023-01-21 14:12:31 +1300

    Created 3 new routes for ideas, meetings and minions

--- server/api.js
+++ server/api.js
@@ -1,6 +1,12 @@
-const express = require('express');
+const express = require("express");
 const apiRouter = express.Router();
 
+const ideasRouter = require("./Routes/ideas");
+const meetingsRouter = require("./Routes/meetings");
+const minionsRouter = require("./Routes/minions");
 
+apiRouter.use("/ideas", ideasRouter);
+apiRouter.use("/meetings", meetingsRouter);
+apiRouter.use("/minions", minionsRouter);
 
 module.exports = apiRouter;
