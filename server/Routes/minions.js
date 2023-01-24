7d0109a4132d61762eb43469e681697c28a17f91
Stanley Mair <stanleygmair@gmail.com>
2023-01-25 06:07:57 +1300

    Finished the basic routes of the minions endpoints

--- server/Routes/minions.js
+++ server/Routes/minions.js
@@ -7,26 +7,39 @@ minionsRouter
 	.get((req, res) => {
 		const allMinions = dbHelperFunctions.getAllFromDatabase("minions");
 		res.send(allMinions);
-		console.log("minions get request received and sent");
 	})
 	.post((req, res) => {
-		// res.send("minions post is working");
 		const requestBody = req.body;
-		console.log(req.body);
-		const newMinion = dbHelperFunctions.addToDatabase("minion", requestBody);
-		console.log(newMinion);
+		requestBody.salary = Number(requestBody.salary);
+		const newMinion = dbHelperFunctions.addToDatabase("minions", requestBody);
 		res.send(newMinion);
 	});
+
+minionsRouter.use("/:minionId", (req, res, next) => {
+	const singleMinion = dbHelperFunctions.getFromDatabaseById(
+		"minions",
+		req.params.minionId
+	);
+	if (!singleMinion) {
+		return res.status(404).send("Minion with that Id does not exist");
+	}
+	req.minion = singleMinion;
+	next();
+});
 
 minionsRouter
 	.route("/:minionId")
 	.get((req, res) => {
-		res.send("minionId get is working");
+		res.send(req.minion);
 	})
 	.put((req, res) => {
-		res.send("minionId put is working");
+		const requestBody = req.body;
+		requestBody.salary = Number(requestBody.salary);
+		req.minion = requestBody;
+		res.send(req.minion);
 	})
 	.delete((req, res) => {
-		res.send("minionId delete is working");
+		req.minion = null;
+		res.status(204).send("Minion has been removed");
 	});
 module.exports = minionsRouter;
