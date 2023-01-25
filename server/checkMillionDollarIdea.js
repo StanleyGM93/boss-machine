92f8bfba05297e63f47b6e5517f98aef498654ce
Stanley Mair <stanleygmair@gmail.com>
2023-01-26 05:16:59 +1300

    Completed the checkMillionDollarIdea func

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,8 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (numWeeks, weeklyRevenue) => {
+	const expectedReturn = numWeeks * weeklyRevenue;
+	const oneMillionDollars = 1000000;
+	return expectedReturn >= oneMillionDollars;
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
