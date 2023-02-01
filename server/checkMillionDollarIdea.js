0f5f567722a14d2cf4e4ef000793b9c261c0e67b
Stanley Mair <stanleygmair@gmail.com>
2023-02-01 13:43:03 +1300

    Made minor changes to comments

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -13,5 +13,4 @@ const checkMillionDollarIdea = (req, res, next) => {
 	}
 };
 
-// Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
